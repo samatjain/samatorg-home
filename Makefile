a466df5e261705e7c167576026e828b246be3a73
Samat K Jain <samat@samat.org>
2015-05-09 19:19:42 -0600

    Merge Makefile contents into fabfile

--- Makefile
+++
@@ -1,6 +0,0 @@
-all:
-	rm -rf output
-	node_modules/.bin/broccoli build output
-
-clean:
-	rm -rf output tmp
--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,20 @@
 import os
 
-from fabric.api import cd, env, local, parallel, run, settings
+from fabric.api import cd, env, local, parallel, run, settings, task
 from fabric.contrib.project import rsync_project
 
 env.hosts = ['claudius']
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-@parallel
+@task
+def clean():
+    local('rm -rf output tmp')
+
+@task
 def deploy():
-    local('make')
+    local('rm -rf output')
+    local('node_modules/.bin/broccoli build output')
     remote_path = '/home/xjjk/sites/samat.org/public'
     rsync_project(
             local_dir= base_dir + '/output/',
